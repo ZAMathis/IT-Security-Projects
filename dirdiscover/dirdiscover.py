fa5572d86c1857e02e9eed2ef94c69399004ed13
Zach Mathis <ZachAMathis@gmail.com>
2025-08-28 21:37:13 -0400

    add pseudo code

--- dirdiscover/dirdiscover.py
+++ dirdiscover/dirdiscover.py
@@ -3,10 +3,31 @@ Web app to test:
     http://10.0.0.217/DVWA/
 """
 
-import requests as req
+import requests
 
-target_url = input("Enter host to scan: ")
+wordlist = open('common_web_dirs.txt', 'r')
 
-request = req.get(target_url)
+# target_url = input("Enter host to scan: ")
 
-print(request.status_code)
+for word in wordlist:
+    print(word)
+
+# request = requests.get(target_url)
+
+# print(request.status_code)
+
+"""
+have user input url
+
+initialize wordlist
+
+def dir_search(url)
+    for each word in the wordlist
+        new url = input_url + word appended
+
+        if request_of_new_url.status = ok
+            print directory found (word)
+            
+            dir_search(new_url)
+
+"""
