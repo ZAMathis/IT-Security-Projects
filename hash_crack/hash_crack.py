510554987ee611b505c830cd3055024a9387dcf9
Zach M <zachamathis@gmail.com>
2025-09-21 14:57:10 -0700

    [hash_crack] added more encryption types

--- hash_crack/hash_crack.py
+++ hash_crack/hash_crack.py
@@ -1,16 +1,18 @@
+import argparse
 import hashlib
 
 """
 TODO:
-add argparse
-    add arguements for hashtype, for multiple hashes in a file, and custom wordlists
-add more encryption types
+add argparse <-- DONE
+    add arguments for hashtype <-- DONE 
+    for multiple hashes in a file 
+    custom wordlists <-- DONE
+add more encryption types <-- IN PROGRESS
+add ability to use a list of hashes
 """
 
-TEST_HASH = hashlib.sha1("LuckyCharm".encode()).hexdigest()
 
-
-# Combining compare functions
+# compare function
 def compare_hash(user_hash, word, hash_type):
     match (hash_type):
         case "md5":
@@ -20,6 +22,35 @@ def compare_hash(user_hash, word, hash_type):
         case "sha1":
             return (
                 True if hashlib.sha1(word.encode()).hexdigest() == user_hash else False
+            )
+        case "sha224":
+            return (
+                True
+                if hashlib.sha224(word.encode()).hexdigest() == user_hash
+                else False
+            )
+        case "sha256":
+            return (
+                True
+                if hashlib.sha256(word.encode()).hexdigest() == user_hash
+                else False
+            )
+        case "sha384":
+            return (
+                True
+                if hashlib.sha384(word.encode()).hexdigest() == user_hash
+                else False
+            )
+        case "sha512":
+            return (
+                True
+                if hashlib.sha512(word.encode()).hexdigest() == user_hash
+                else False
+            )
+
+        case _:
+            print(
+                "Invalid encryption type. Could not be added to program, but check your spelling"
             )
 
 
@@ -45,9 +76,32 @@ def check_hash(user_hash, wordlist, hash_type):
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Command line tool written in python to crack hashes"
+    )
 
-    with open("passwords.txt", "r") as wl:
-        pass_list = [line.strip() for line in wl.readlines() if line.strip()]
-        print(f"loaded {len(pass_list)} words from passwords.txt")
+    parser.add_argument("-t", "--target", help="Target hash to crack")
 
-    check_hash(TEST_HASH, pass_list, "sha1")
+    parser.add_argument(
+        "-w",
+        "--wordlist",
+        help="Dictionary/wordlist to use to crack the hash",
+        default="passwords.txt",
+    )
+
+    parser.add_argument(
+        "--hashtype",
+        help="Type of hash that you're trying to crack. I.e md5, sha1, etc",
+    )
+
+    args = parser.parse_args()
+
+    try:
+        with open(args.wordlist, "r") as wl:
+            wordlist = [line.strip() for line in wl.readlines() if line.strip()]
+            print(f"Loaded {len(wordlist)} lines from {args.wordlist}")
+    except FileNotFoundError:
+        print("Could not find provided wordlist, exiting...")
+        exit()
+
+    check_hash(args.target, wordlist, args.hashtype)
