9546e4ecb15e070dbbcb1b1c54c62e0e0dce1fa1
Zach M <zachamathis@gmail.com>
2025-09-23 20:25:17 -0700

    [hash_crack] use list of hashes, check_hash clean

--- hash_crack/hash_crack.py
+++ hash_crack/hash_crack.py
@@ -4,11 +4,14 @@ import hashlib
 """
 TODO:
 add argparse <-- DONE
-    add arguments for hashtype <-- DONE 
-    for multiple hashes in a file 
+    add arguments for hashtype <-- DONE  
     custom wordlists <-- DONE
-add more encryption types <-- IN PROGRESS
-add ability to use a list of hashes
+add more encryption types <-- DONE (kind of)
+add ability to use a list of hashes <-- IN PROGRESS
+add option to save output of results to a file
+add ability to actually FIND OUT what type of hash is inputted
+
+there is a bug that if the user is decrypting a list, it will print out "Nope" at the top
 """
 
 
@@ -58,21 +61,12 @@ def check_hash(user_hash, wordlist, hash_type):
     is_found = False
 
     for word in wordlist:
-        match (hash_type):
-            case "md5":
-                if compare_hash(user_hash, word, hash_type):
-                    print(f"Password found! {word}, {hash_type}")
-                    is_found = True
-                    break
-
-            case "sha1":
-                if compare_hash(user_hash, word, hash_type):
-                    print(f"Password found! {word}, {hash_type}")
-                    is_found = True
-                    break
+        if compare_hash(user_hash, word, hash_type):
+            print(f"Password found! {word}, {hash_type}")
+            is_found = True
 
     if not is_found:
-        print("Nope")
+        print("Password not found with current hashtype/wordlist!")
 
 
 if __name__ == "__main__":
@@ -83,15 +77,20 @@ if __name__ == "__main__":
     parser.add_argument("-t", "--target", help="Target hash to crack")
 
     parser.add_argument(
-        "-w",
+        "-wl",
         "--wordlist",
         help="Dictionary/wordlist to use to crack the hash",
         default="passwords.txt",
     )
 
     parser.add_argument(
+        "-ht",
         "--hashtype",
         help="Type of hash that you're trying to crack. I.e md5, sha1, etc",
+    )
+
+    parser.add_argument(
+        "-f", "--file", help="Target file with a list of hashes to crack"
     )
 
     args = parser.parse_args()
@@ -100,8 +99,19 @@ if __name__ == "__main__":
         with open(args.wordlist, "r") as wl:
             wordlist = [line.strip() for line in wl.readlines() if line.strip()]
             print(f"Loaded {len(wordlist)} lines from {args.wordlist}")
+            check_hash(args.target, wordlist, args.hashtype)
     except FileNotFoundError:
         print("Could not find provided wordlist, exiting...")
         exit()
 
-    check_hash(args.target, wordlist, args.hashtype)
+    if args.file:
+        try:
+            with open(args.file, "r") as target_file:
+                target_list = [
+                    line.strip() for line in target_file.readlines() if line.strip()
+                ]
+                for line in target_list:
+                    check_hash(line, wordlist, args.hashtype)
+        except FileNotFoundError:
+            print("Target file not found, check your spelling. Exiting...")
+            exit()
